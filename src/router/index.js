c7fdfd99c4ea3fcb9359a6c510817db4071dbd28
Carlos Eduardo Ilgenfritz <ilgenfritzc@gmail.com>
2020-04-24 19:10:52 -0400

    subindo rota de login

--- src/router/index.js
+++ src/router/index.js
@@ -4,16 +4,23 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import adm from '@/components/adm'
 import verifica from '@/components/verifica'
+import Login from '@/components/login'
 Vue.use(Router)
 
 export default new Router({
     routes: [{
-        path: '/',
-        name: 'adm',
-        component: adm
-    }, {
-        path: '/verifica',
-        name: 'verifica',
-        component: verifica
-    }]
+            path: '/',
+            name: 'adm',
+            component: adm
+        }, {
+            path: '/verifica',
+            name: 'verifica',
+            component: verifica
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: Login
+        }
+    ]
 })
